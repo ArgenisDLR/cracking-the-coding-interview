f0206c1180a2410141b63351c69a48624dacf857
Argenis De La Rosa <oftherose91@gmail.com>
2019-08-18 11:57:20 -0400

    Create palindrom-permutation.swift

---
+++ palindrom-permutation.swift
@@ -0,0 +1,26 @@
+func isPalindromePermutation(word: String) -> Bool {
+  // create an empty Set of Characters
+  var oddCharacters: Set<Character> = []
+  
+  // iterating over the array
+  for char in word {
+    // checking if the array contains the element
+    if oddCharacters.contains(char) {
+      // remove it if it does
+      oddCharacters.remove(char)
+    } else {
+      // else , insert the element
+      oddCharacters.insert(char)
+    }
+  }
+  // return length of the array if it's less than or equal to 1
+  return oddCharacters.count <= 1
+}
+
+// True
+print(isPalindromePermutation(word: "racecar"))
+print(isPalindromePermutation(word: "hannah"))
+
+// False
+print(isPalindromePermutation(word: "truck"))
+print(isPalindromePermutation(word: "habits"))
